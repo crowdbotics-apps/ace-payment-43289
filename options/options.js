fa7716da89d69044c18f650d4061cba393869188
Crowdbotics <team@crowdbotics.com>
2023-09-08 21:28:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-boat-43289",
+  url: "https://damp-boat-43289.botics.co",
+  api: "https://damp-boat-43289.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
